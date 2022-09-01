d4931e3b7ac015d06896c0ee2051b25dbf8fa48f
Niyolynate <niolynate@gmail.com>
2022-09-01 11:15:02 +0300

    added some operations

--- cal.py
+++ cal.py
@@ -1,10 +1,36 @@
-a = int(input('enter the first number'))
-b = int(input('enter the second number'))
+def add(a, b):
+    return a + b
 
-print(a, b end=" ")
-while n-2:
-    c = a+b
-    a = b
-    b = c
-    print(c, end=" ")
-    n= n-1
+
+def sub(a, b):
+    return a - b
+
+
+def mul(a, b):
+    return a * b
+
+
+def divide(a, b):
+    return a/b
+
+
+def si(p,r,t):
+    return (p*r*t) /100
+
+
+def ci(P,R,T):
+    return P * pow((1 + R/100), T)
+
+
+def sqr(num):
+    return num**2
+ 
+
+def st(num):
+    return num ** 0.5
+
+
+print(st(64):
+        return num **0.5
+
+print(st(64))
