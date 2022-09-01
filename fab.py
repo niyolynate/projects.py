6b716ed166abb59c497a6ab3b2648eb162ac16fb
Niyolynate <niolynate@gmail.com>
2022-09-01 10:41:58 +0300

    added fab3

---
+++ fab.py
@@ -0,0 +1,11 @@
+a = int(input('enter the first number'))
+b = int(input('enter the second number'))
+
+print(a, b end=" ")
+while n-2:
+    c = a+b
+    a = b
+    b = c
+    print(c, end=" ")
+    n= n-1
+
